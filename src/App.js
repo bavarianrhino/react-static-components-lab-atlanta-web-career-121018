0e1442d5ac07b6b6859113cab640f48288e5818e
github username <github email address>
2019-02-19 14:24:10 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js';
-import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
+import EinsteinQuoteComponent from './EinsteinQuoteComponent';
 import MouseComponent from './MouseComponent';
 
 class App extends Component {
@@ -8,9 +8,13 @@ class App extends Component {
 		// your code in the return statement below!
 		return (
 			<div className="App">
+				<CatComponent />
+				<EinsteinQuoteComponent />
+				<MouseComponent />
 			</div>
 		);
 	}
 }
+
 
 export default App;
